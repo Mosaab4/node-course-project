29cb25d54afc5bde0e35654f8b0486ccb5cfaae3
Mosaab <musab.ehab@gmail.com>
2023-04-19 20:21:59 +0200

    add html files

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/")
-})
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/', (req, res, next) => {
-    console.log("In another middleware")
-    res.send('<h1>Hello</h1>')
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
