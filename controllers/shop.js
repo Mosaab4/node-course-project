2c8ab7e533bc2cf12648711036fa991844d7ff79
Mosaab <musab.ehab@gmail.com>
2023-04-20 20:59:27 +0200

    add delete cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -22,9 +22,28 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getProducts(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = []
+
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id)
+                if (cart.products.find(prod => prod.id === product.id)) {
+                    if (cartProductData) {
+                        cartProducts.push({
+                            productData: product,
+                            qty: cartProductData.qty
+                        })
+                    }
+                }
+            }
+
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            })
+        })
     })
 }
 
@@ -60,6 +79,15 @@ exports.postCart = (req, res, next) => {
     Product.findById(productId, product => {
         Cart.addProduct(product.id, product.price)
     })
-    
+
     res.redirect('/cart')
+}
+
+exports.postCartDelete = (req, res, next) => {
+    const prodId = req.body.productId
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price)
+        res.redirect('/cart')
+    })
+
 }
