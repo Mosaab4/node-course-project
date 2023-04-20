543c0fec8d1791c3becd0651135c58cd430505f6
Mosaab <musab.ehab@gmail.com>
2023-04-20 02:22:49 +0200

    add controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const errorController = require('./controllers/error')
 
 const app = express()
 
@@ -14,12 +15,10 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 
-app.use('/admin', adminRoutes.router)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.use(errorController.get404)
 
 app.listen(3000)
