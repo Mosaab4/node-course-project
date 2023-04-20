945c1c7a50ed38674637e93472557fad676c40c0
Mosaab <musab.ehab@gmail.com>
2023-04-20 02:08:47 +0200

    add templates

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -11,7 +14,7 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminRoutes.router)
 app.use(shopRoutes)
 
 
