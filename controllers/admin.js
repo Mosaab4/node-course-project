627aa790b3c1e2f476ff283e1ef2864951b4bcea
Mosaab <musab.ehab@gmail.com>
2023-04-23 22:00:40 +0200

    add mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,9 @@ const Product = require('../models/product')
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
-        pageTitle: 'Add Product', path: '/admin/add-product', editing: false
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
     })
 }
 
@@ -11,7 +13,13 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const price = req.body.price
     const description = req.body.description
-    const product = new Product(title, price, description, imageUrl, null, req.user._id)
+    const product = new Product({
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+        userId: req.user
+    })
     product
         .save()
         .then(result => {
@@ -31,13 +39,15 @@ exports.getEditProduct = (req, res, next) => {
     }
     const prodId = req.params.productId
     Product.findById(prodId)
-        // Product.findById(prodId)
         .then(product => {
             if (!product) {
                 return res.redirect('/')
             }
             res.render('admin/edit-product', {
-                pageTitle: 'Edit Product', path: '/admin/edit-product', editing: editMode, product: product
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
             })
         })
         .catch(err => console.log(err))
@@ -50,9 +60,14 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl
     const updatedDesc = req.body.description
 
-    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId)
-    product
-        .save()
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle
+            product.price = updatedPrice
+            product.description = updatedDesc
+            product.imageUrl = updatedImageUrl
+            return product.save()
+        })
         .then(result => {
             console.log('UPDATED PRODUCT!')
             res.redirect('/admin/products')
@@ -61,10 +76,15 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
+        // .select('title price -_id')
+        // .populate('userId', 'name')
         .then(products => {
+            console.log(products)
             res.render('admin/products', {
-                prods: products, pageTitle: 'Admin Products', path: '/admin/products'
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
             })
         })
         .catch(err => console.log(err))
@@ -72,7 +92,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
         .then(() => {
             console.log('DESTROYED PRODUCT')
             res.redirect('/admin/products')
