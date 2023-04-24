91db3f596aed5979cc65f3d51317fa6732bb9720
Mosaab <musab.ehab@gmail.com>
2023-04-24 04:39:59 +0200

    remove variable

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,6 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.session.isLoggedIn,
     })
 }
 
